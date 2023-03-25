42345376c3f51a2b79213e9f806b2ec1ef497b0d
Paulchrr <100359031+Paulchrr@users.noreply.github.com>
2023-03-25 19:19:55 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,21 +8,17 @@ import subprocess
 from bs4 import BeautifulSoup
 
 
-# ------------------------------------------------------------------
-#API
+# ------------------------------------------------------------------ #
+#Clé API (https://securitytrails.com)
 securitytrails_API = "8DLoeiTT0NPN4dW98we1NqjQHOja3USR"
 
-
-# ------------------------------------------------------------------
-
+# ------------------------------------------------------------------ #
+#Fonction qui demande à l'utilisateur le nom de domaine cible
 def choose_domain():
     global domain_target
     domain_target = input("Indiquer le nom de domaine cible ? : ")
 
-# ------------------------------------------------------------------
-#Résultats output
-
-
+# ------------------------------------------------------------------ #
 #Création du fichier contenant les résultats
 def create_result():
     global newpath
@@ -45,11 +41,8 @@ def print_result_file():
     with open(RESULT_FILE, 'r') as f:
         data = f.read()
         print(data)
-# ------------------------------------------------------------------
-
-# ------------------------------------------------------------------
-#FUNCTION GENERIQUE
-# ------------------------------------------------------------------
+# ------------------------------------------------------------------ #
+#Fonction qui va chercher des mails à partir du nom de domaine donné par l'utilisateur
 def search_mail():
     try : 
         result_google = get_emails_from_google(domain_target)
@@ -72,8 +65,10 @@ def search_mail():
                 print(i)
                 f.write(i+'\n')
     except:
+        print("Le module search_mail() n'a pas fonctionné")
         pass
 
+# ------------------------------------------------------------------ #
 #Fonction qui sera utilisé pour executer des commandes en subprocess
 def execute(command):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
@@ -88,13 +83,15 @@ def execute(command):
         print('La commande à échoué')
         pass
 
-#Fonction qui ajoute automatiquement le https
+# ------------------------------------------------------------------ #
+#Fonction qui ajoute automatiquement le https.
+    #Cette fonction est nécessaire car certaines fonctions utiliserons l'https.
 def add_https(url):
     if not url.startswith("https://"):
         url = "https://" + url
     return url
 
-# ------------------------------------------------------------------
+# ------------------------------------------------------------------ #
 #Fonction qui va nous afficher les dernières CVE 
     #L'api utilisé est https://cve.circl.lu/api/last/
     
@@ -112,7 +109,7 @@ def get_latest_cve(num_cve):
             print("{:<50}{}{}".format(cve1_print, "  |  " if cve2 else "", cve2_print))
     else:
         print(f"Erreur lors de la requête : {response.status_code}")
-# ------------------------------------------------------------------
+# ------------------------------------------------------------------ #
 
 #Fonction qui check si un site est sur wordpress
 def is_wordpress():
@@ -123,6 +120,7 @@ def is_wordpress():
     from colorama import Fore,init
     init()
 
+    #Fonction qui va scanner un site wordpress
     def scan_wordpress():
 
         with open(RESULT_FILE,'a') as f:
@@ -131,7 +129,7 @@ def is_wordpress():
 
             user_agent_list = ["Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 7.0; SM-G930VC Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 6.0.1; SM-G935S Build/MMB29K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 6.0.1; Nexus 6P Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 7.1.1; G8231 Build/41.2.A.0.219; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 6.0.1; E6653 Build/32.2.A.0.253) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 6.0; HTC One X10 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 6.0; HTC One M9 Build/MRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.3","Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1","Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/69.0.3497.105 Mobile/15E148 Safari/605.1","Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/13.2b11866 Mobile/16A366 Safari/605.1.15","Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1","Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1","Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A5370a Safari/604.1","Mozilla/5.0 (iPhone9,3; U; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1","Mozilla/5.0 (iPhone9,4; U; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1","Mozilla/5.0 (Apple-iPhone7C2/1202.466; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543 Safari/419.3","Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15254","Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; RM-1127_16056) AppleWebKit/537.36(KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10536","Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 950) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Mobile Safari/537.36 Edge/13.1058","Mozilla/5.0 (Linux; Android 7.0; Pixel C Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.98 Safari/537.36","Mozilla/5.0 (Linux; Android 6.0.1; SGP771 Build/32.2.A.0.253; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.98 Safari/537.36","Mozilla/5.0 (Linux; Android 6.0.1; SHIELD Tablet K1 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Safari/537.36","Mozilla/5.0 (Linux; Android 7.0; SM-T827R4 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Safari/537.36","Mozilla/5.0 (Linux; Android 5.0.2; SAMSUNG SM-T550 Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/3.3 Chrome/38.0.2125.102 Safari/537.36","Mozilla/5.0 (Linux; Android 4.4.3; KFTHWI Build/KTU84M) AppleWebKit/537.36 (KHTML, like Gecko) Silk/47.1.79 like Chrome/47.0.2526.80 Safari/537.36","Mozilla/5.0 (Linux; Android 5.0.2; LG-V410/V41020c Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/34.0.1847.118 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246","Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1","Mozilla/5.0 (CrKey armv7l 1.5.16041) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.0 Safari/537.36","Roku4640X/DVP-7.70 (297.70E04154A)","Mozilla/5.0 (Linux; U; Android 4.2.2; he-il; NEO-X5-116A Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30","Mozilla/5.0 (Linux; Android 5.1; AFTS Build/LMY47O) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/41.99900.2250.0242 Safari/537.36","Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus Player Build/MMB29T)","AppleTV6,2/11.1","AppleTV5,3/9.1.1","Mozilla/5.0 (Nintendo WiiU) AppleWebKit/536.30 (KHTML, like Gecko) NX/3.0.4.2.12 NintendoBrowser/4.3.1.11264.US","Mozilla/5.0 (Windows NT 10.0; Win64; x64; XBOX_ONE_ED) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393","Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Xbox; Xbox One) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Mobile Safari/537.36 Edge/13.10586","Mozilla/5.0 (PlayStation 4 3.11) AppleWebKit/537.73 (KHTML, like Gecko)","Mozilla/5.0 (PlayStation Vita 3.61) AppleWebKit/537.73 (KHTML, like Gecko) Silk/3.2","Mozilla/5.0 (Nintendo 3DS; U; ; en) Version/1.7412.EU","Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)","Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)","Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)","Mozilla/5.0 (X11; U; Linux armv7l like Android; en-us) AppleWebKit/531.2+ (KHTML, like Gecko) Version/5.0 Safari/533.2+ Kindle/3.0+","Mozilla/5.0 (Linux; U; en-US) AppleWebKit/528.5+ (KHTML, like Gecko, Safari/528.5+) Version/4.0 Kindle/3.0 (screen 600x800; rotate)"]
             RANDOM_USER_AGENT = user_agent_list[randint(0,52)]
-            #------------------------------------------------------------------
+            
             def user_finder(new_u) :
 
                 new_url2 = new_u+'/wp-json/wp/v2/users'
@@ -150,8 +148,8 @@ def is_wordpress():
                         time.sleep(0.2)
                 else :
                         print('\n[-] Usernames Not Found ')
-            #------------------------------------------------------------------
-
+            
+            #fonction qui va trouver le panel d'admin du wordpress
             def adminpanel_finder(org_url) :
                 
                 urlA = org_url+'/wp-login.php?action=lostpassword&error=invalidkey'
@@ -183,7 +181,6 @@ def is_wordpress():
                     print('\n[-] Admin panel not found ')
                     f.write('\n[-] Admin panel not found \n ')
 
-            #---------------------------------------------
             #print( '\nWebsite Url (with https://) : ', end="")
             url = add_https(domain_target)
             org_url = url
@@ -305,14 +302,14 @@ def is_wordpress():
                 print("choix invalide")
 
 
-# ------------------------------------------------------------------
-
-# ------------------------------------------------------------------
+# ------------------------------------------------------------------ #
+ #Utilisation de DuckDuckGO pour les "google dorks". Voici le wiki : https://help.duckduckgo.com/duckduckgo-help-pages/results/syntax/
+    # Doc utilisés : 
+        #  https://www.makeuseof.com/duckduckgo-get-faster-results-search-operators/
+        #  https://brettterpstra.com/2019/03/07/the-ultimate-guide-to-duckduckgo/
+        
 def dorks(domaine):
-    #Utilisation de DuckDuckGO pour les "google dorks". Voici le wiki : https://help.duckduckgo.com/duckduckgo-help-pages/results/syntax/
-        # Doc utilisés
-            #  https://www.makeuseof.com/duckduckgo-get-faster-results-search-operators/
-            #  https://brettterpstra.com/2019/03/07/the-ultimate-guide-to-duckduckgo/
+    #Le but de la fonction est d'aller chercher des fichiers pdf ou xls qui sont hébergé par le domaine ciblé
     def search_dorks(domaine, filetype):
         query = f'site:{domaine} filetype:{filetype}'
         url = f'https://duckduckgo.com/html?q={query}'
@@ -338,11 +335,11 @@ def dorks(domaine):
         print("pas de documents trouvé")
 
     return 'Les résultats de la recherche ont été enregistrés dans le fichier resultat.txt'
-# ------------------------------------------------------------------
-# ------------------------------------------------------------------
-#FUNCTION AUTO MODE
+
 # ------------------------------------------------------------------
 
+#La fonction à pour but de réaliser un who is à partir du domaine indiqué.
+#Nous utilisons l'API http://ipwhois.app/json
 def auto_whois():
     import dns.resolver
     time.sleep(1)
@@ -411,7 +408,10 @@ def auto_whois():
         else:
             print("\nError: Invalid IP Address.\n")
             continue
-# ------------------------------------------------------------------
+# ------------------------------------------------------------------ #
+
+#La fonction à pour but de trouver les sous domaines à partir du domaine cible
+#Nous utilons ici l'API securityTrails.com
 def get_sub_domains(domain_target,RESULT_FILE):
     try :
         url_subdomain = "https://api.securitytrails.com/v1/domain/"+domain_target+"/subdomains"
@@ -424,6 +424,7 @@ def get_sub_domains(domain_target,RESULT_FILE):
         result_json=json.loads(response.text)
         sub_domains=[i+'.'+domain_target for i in result_json['subdomains']]
         
+        #Nous allons ici écrire les résultats dans notre RESULT_FILE
         with open(RESULT_FILE,'a') as f:
             print("Les sous domaines")
             print(("-"*30 + '\n'))
@@ -439,7 +440,9 @@ def get_sub_domains(domain_target,RESULT_FILE):
         print("Un problème avec le module des sous domaines, peux-être que la clé API a atteint son maximum de requêtes, dans ce cas veuillez actualiser la clé API securitytrails_API")
         pass
         
-# ------------------------------------------------------------------
+# ------------------------------------------------------------------ #
+
+#Fonction qui va réaliser un NMAP avec les arguments -sV
 def auto_nmap():
     options = ["Oui", "Non"]
     print("")
@@ -474,8 +477,8 @@ def auto_nmap():
             print(print_result)
     else:
         pass
-# ------------------------------------------------------------------
-
+# ------------------------------------------------------------------ #
+#Fonction AUTO qui va appeler les autres fonctions utilisés par ce module
 def auto():
     os.system('clear')
     print("""\
@@ -504,8 +507,9 @@ def auto():
     print("Tous les résultats sont stockés dans : " +'\n')
     print(RESULT_FILE)
     print(RESULT_NMAP)
-# ------------------------------------------------------------------
-
+    
+# ------------------------------------------------------------------ #
+#Fonction OSINT qui va appeler les autres fonctions utilisés par ce module
 def osint():
     choose_domain()
     create_result()
